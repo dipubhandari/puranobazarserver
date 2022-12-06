06a8e14fb80ae2b262e66e2e67e9050b87fc48bb
dipubhandari <dipendrabhandari123456@gmail.com>
2022-12-07 01:43:43 +0545

    deploy code puranopazar

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ import express from "express";
 import dbcon from "./dbcon/dbcon.js";
 import router from "./routes/web.js";
 import cors from 'cors'
+import path from "path";
 import { join } from "path";
 // defining variables
 const app = express()
 const PORT = '5000'
 const DATABASE_URL = process.env.PORT || "mongodb://localhost:27017"
-
+app.use(express.static(path.join(process.cwd(), './clientpb/build')))
 // database connect
 dbcon(DATABASE_URL)
-
+// 
+app.get('*', (req, res) => {
+    res.send((path.join(process.cwd(), './clientpb/build/index.html')))
+})
 app.use(cors())
 
 // set engine
